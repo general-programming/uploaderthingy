fdbf47f4e904e8750418910b1d537566fb225543
nepeat <nepeat@gmail.com>
2019-07-29 07:08:53 -0700

    Move Core Data code to the helper class.

--- uploaderthingy/Helpers/CoreDataHelper.swift
+++ uploaderthingy/Helpers/CoreDataHelper.swift
@@ -8,14 +8,57 @@
 
 import UIKit
 import CoreData
+import Social
 
 struct CoreDataHelper {
-    static let context: NSManagedObjectContext = {
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            fatalError()
-        }
+    // MARK: - Core Data stack
 
-        let persistentContainer = appDelegate.persistentContainer
+    static var persistentContainer: NSPersistentCloudKitContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+        */
+        let container = NSPersistentCloudKitContainer(name: "UploaderModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                 
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    static func saveContext () {
+        print("Saving CoreData context.")
+        let context = CoreDataHelper.persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    static let context: NSManagedObjectContext = {
+        let persistentContainer = CoreDataHelper.persistentContainer
         let context = persistentContainer.viewContext
 
         return context
